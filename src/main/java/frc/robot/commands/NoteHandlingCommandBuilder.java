93e891e51e3887f6beb6778e0b0834884be657ce
Ares Kittell <akittell2025@francisparker.org>
2024-11-07 17:32:43 -0800

    added 104 training

--- src/main/java/frc/robot/commands/NoteHandlingCommandBuilder.java
+++ src/main/java/frc/robot/commands/NoteHandlingCommandBuilder.java
@@ -1,20 +1,168 @@
-
 package frc.robot.commands;
-import java.util.function.Supplier;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.NoteHandling.GeneralRoller;
+import frc.robot.subsystems.NoteHandling.GeneralRoller.GeneralRollerStates;
+import frc.robot.subsystems.NoteHandling.Shooter;
+import frc.robot.subsystems.NoteHandling.Shooter.ShooterStates;
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
+import frc.robot.subsystems.NoteHandling.Pivot;
+import frc.robot.subsystems.NoteHandling.Pivot.PivotStates;
+import frc.robot.subsystems.NoteHandling.Intake;
+import frc.robot.subsystems.NoteHandling.Intake.IntakeStates;
 
 // We are gonna be writing commands today, use this doc to progress through the training:
 // https://docs.google.com/document/d/1iaDaoYRCIEgX1hY3d1hFmAgDCdyEqP5SY_TVuxs04kA/edit?tab=t.0
 
 public class NoteHandlingCommandBuilder {
 
-    public static Command generalRollerRunForward(/* add parameters here, you will need something pretty important */){
-
-        // Instantiate a command/command group here, then return it. Remember to use the keyword "new" when instantiating an object
-
-        return /* command */;
-
+    public static Command generalRollerRunForward(GeneralRoller generalRoller) {
+        return new InstantCommand(() -> generalRoller.requestState(GeneralRollerStates.StateForward), generalRoller);
+    }
+    
+    public static Command generalRollerRunReverse(GeneralRoller generalRoller) {
+        return new InstantCommand(() -> generalRoller.requestState(GeneralRollerStates.StateReverse), generalRoller);
     }
 
-    // Repeat above for the other 2 commands specified in the doc
+    public static Command generalRollerStop(GeneralRoller generalRoller) {
+        return new InstantCommand(() -> generalRoller.requestState(GeneralRollerStates.StateOff), generalRoller);
+    }    
 
+
+    public static Command runFeeder(GeneralRoller feeder, GeneralRoller indexer) {
+        Command command = new ParallelCommandGroup(
+                                new RunCommand(()->feeder.requestState(GeneralRollerStates.StateForwardFast), feeder),
+                                new RunCommand(()->indexer.requestState(GeneralRollerStates.StateForwardFast), indexer)                 
+                                );
+        return command;
+    }
+
+    public static Command feederOff(GeneralRoller feeder, GeneralRoller indexer) {
+        Command command = new ParallelCommandGroup(
+                        new InstantCommand(()->feeder.requestState(GeneralRollerStates.StateOff), feeder),
+                        new InstantCommand(()->indexer.requestState(GeneralRollerStates.StateOff), indexer)                 
+                        );
+        return command;
+    }
+
+    public static Command shooterSpeaker(Shooter shooter, GeneralRoller feeder, GeneralRoller indexer){
+        Command command = new SequentialCommandGroup(
+            new RunCommand(()->shooter.requestState(ShooterStates.StateSpeaker), shooter).until(()->shooter.getCurrentState()==ShooterStates.StateSpeaker),
+            runFeeder(feeder, indexer)
+            );
+
+        return command;
+    }
+
+    public static Command shooterPasser(Shooter shooter, GeneralRoller feeder, GeneralRoller indexer){
+        Command command = new SequentialCommandGroup(
+            new RunCommand(()->shooter.requestState(ShooterStates.StatePass), shooter).until(()->Math.abs(shooter.getVelocity()-50)<8),
+            runFeeder(feeder, indexer)
+            );
+            
+
+        return command;
+    }
+
+
+    public static Command DIAShoot(Shooter shooter, GeneralRoller feeder, GeneralRoller indexer){
+        Command command = new ParallelCommandGroup(
+            new RunCommand(()->shooter.requestState(ShooterStates.StatePass), shooter), runFeeder(feeder, indexer)
+        );
+        return command;
+    }
+
+    public static Command shooterOff(Shooter shooter, GeneralRoller feeder, GeneralRoller indexer){
+        Command command = new ParallelCommandGroup(
+            new InstantCommand(()->shooter.requestState(ShooterStates.StateOff), shooter),
+            feederOff(feeder, indexer));
+        return command;
+    }
+
+    public static Command shooterCoast(Shooter shooter, GeneralRoller feeder, GeneralRoller indexer){
+        Command command = new ParallelCommandGroup(
+            new InstantCommand(()->shooter.requestState(ShooterStates.StateCoast), shooter));
+            //feederOff(feeder, indexer));
+        return command;
+    }
+
+    public static Command autoShooterSpeaker(Pivot pivot, Shooter shooter, GeneralRoller feeder, GeneralRoller indexer) {
+        Command command = new ParallelCommandGroup(
+                        
+                        new RunCommand(()->pivot.requestState(PivotStates.StateShooter), pivot), 
+                        new RunCommand(()->shooter.requestState(ShooterStates.StateSpeaker), shooter),
+                        new RunCommand(()->feeder.requestState(GeneralRollerStates.StateOff), feeder),
+                        new RunCommand(()->indexer.requestState(GeneralRollerStates.StateOff), indexer)
+                        ).until(()->pivot.getCurrentState() == PivotStates.StateShooter && shooter.getCurrentState() == ShooterStates.StateSpeaker)
+                        .andThen(
+                            new ParallelCommandGroup(
+                                new RunCommand(()->feeder.requestState(GeneralRollerStates.StateForwardFast), feeder),
+                                new RunCommand(()->indexer.requestState(GeneralRollerStates.StateForwardFast), indexer)
+                            )
+                
+                        );
+        return command.withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
+    }
+
+    public static Command autoShooterStageSetpoint(Pivot pivot, Shooter shooter, GeneralRoller feeder, GeneralRoller indexer) {
+        Command command = new ParallelCommandGroup(
+                        new RunCommand(()->pivot.requestState(PivotStates.StateStageSetpoint), pivot), 
+                        new RunCommand(()->shooter.requestState(ShooterStates.StateSpeaker), shooter),
+                        new RunCommand(()->feeder.requestState(GeneralRollerStates.StateOff), feeder),
+                        new RunCommand(()->indexer.requestState(GeneralRollerStates.StateOff), indexer)
+                        ).until(()->pivot.getCurrentState() == PivotStates.StateStageSetpoint && shooter.getCurrentState() == ShooterStates.StateSpeaker)
+                        .andThen(
+                            new ParallelCommandGroup(
+                                new RunCommand(()->feeder.requestState(GeneralRollerStates.StateForwardFast), feeder),
+                                new RunCommand(()->indexer.requestState(GeneralRollerStates.StateForwardFast), indexer)
+                            )
+                        );
+        return command.withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
+    }
+ 
+    public static Command autoShooterPodiumSetpoint(Pivot pivot, Shooter shooter, GeneralRoller feeder, GeneralRoller indexer) {
+        Command command = new ParallelCommandGroup(
+                        new RunCommand(()->pivot.requestState(PivotStates.StatePodiumSetpoint), pivot), 
+                        new RunCommand(()->shooter.requestState(ShooterStates.StateSpeaker), shooter),
+                        new RunCommand(()->feeder.requestState(GeneralRollerStates.StateOff), feeder),
+                        new RunCommand(()->indexer.requestState(GeneralRollerStates.StateOff), indexer)
+                        ).until(()->pivot.getCurrentState() == PivotStates.StatePodiumSetpoint && shooter.getCurrentState() == ShooterStates.StateSpeaker)
+                        .andThen(
+                            new ParallelCommandGroup(
+                                new RunCommand(()->feeder.requestState(GeneralRollerStates.StateForwardFast), feeder),
+                                new RunCommand(()->indexer.requestState(GeneralRollerStates.StateForwardFast), indexer)
+                            )
+                        );
+        return command.withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
+    }
+
+    public static Command autoShooterOff(Pivot pivot, Shooter shooter, GeneralRoller feeder, GeneralRoller indexer, Intake intake) {
+        Command command = new ParallelCommandGroup(
+                        new InstantCommand(()->pivot.requestState(PivotStates.StateDown), pivot),
+                        new InstantCommand(()->intake.requestState(IntakeStates.StateOff), intake),
+                        shooterOff(shooter, feeder, indexer)
+                        );
+        return command;
+    }
+
+    public static Command autoShooterOffish(Pivot pivot, Shooter shooter, GeneralRoller feeder, GeneralRoller indexer, Intake intake) {
+        Command command = new ParallelCommandGroup(
+                        new InstantCommand(()->pivot.requestState(PivotStates.StateAutoIntake), pivot),
+                        new InstantCommand(()->intake.requestState(IntakeStates.StateOff), intake),
+                        shooterCoast(shooter, feeder, indexer)
+                        );
+        return command;
+    }
+
+    public static Command shoot(Shooter shooter, GeneralRoller feeder, GeneralRoller indexer) {
+        Command command = new SequentialCommandGroup(
+                        new RunCommand(()->shooter.requestState(ShooterStates.StateSpeaker), shooter).until(()->shooter.getCurrentState() == ShooterStates.StateSpeaker),
+                        runFeeder(feeder, indexer)
+                        );
+
+        return command;
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,106 @@
+package frc.robot.subsystems.NoteHandling;
+
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+// Imports go here
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.ShooterConstants.*;
+
+import java.util.function.DoubleSupplier;
+
+import static frc.robot.Constants.*;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.*;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.controls.*;
+
+// FOLLOW ALONG THIS DOCUMENTATION: https://docs.google.com/document/d/143tNsvYQFAErQTJDxO9d1rwM7pv80vpLfLK-WiIEOiw/edit?tab=t.0
+
+public class Shooter extends SubsystemBase {
+
+    public enum ShooterStates{
+        // MAKE STATES
+        // some considerations: off state, states for shooter at each type of scoring location, and a transition state between states
+
+        // ||||||||||||||||||||||||||||||||
+    }
+
+    public static ShooterStates m_shooterRequestedState;
+    public static ShooterStates m_shooterCurrentState;
+ 
+    // CREATE TALON MOTORS HERE
+    // the shooter has two talon motors on it, have fun
+
+    // ||||||||||||||||||||||||||||||||
+
+    private double desiredVelocity = 0;
+    private double desiredVoltage = 0;
+
+    // you might notice a new type right below here called a "DoubleSupplier," don't worry about it, you won't need to use distanceFromSpeaker for this
+    // incase you were wonder though, it is a lambda, cause of course it is
+    public Shooter(DoubleSupplier distanceFromSpeaker) {
+
+        // CREATE THE CONFIGURATIONS FOR THE TALONS HERE
+        // talon configs are set up differently than sparks, please use the doc if you want to spare your sanity
+        var talonFXConfigs = new TalonFXConfiguration();
+        
+        // ||||||||||||||||||||||||||||||||
+
+        // give some default state to these guys
+        // m_shooterCurrentState;
+        // m_shooterRequestedState;
+
+    }
+        
+    @Override
+    public void periodic() {
+
+
+        // SWITCH/IF STATEMENT GOES HERE
+
+        // ||||||||||||||||||||||||||||||||
+     
+        runControlLoop();
+    
+        // ERROR CHECKING GOES HERE
+
+        // ||||||||||||||||||||||||||||||||
+
+    }
+
+      public void runControlLoop() {
+        // SHOOTER SHENANIGANS GO HERE UNLESS YOU ARE TOO COOL FOR THAT
+
+        // ||||||||||||||||||||||||||||||||
+      }
+    
+      // SO MANY METHODS TO MAKE (like 4), SO LITTLE TIME TO DO IT (literally 6 hours)
+
+      public double getVelocity() {
+        // CHANGE DIS PLZ
+        return 0;
+      }
+    
+      public double getError() {
+        // CHANGE DIS PLZ
+        return 0;
+      }
+     
+      public void requestState(ShooterStates requestedState) {
+        // CHANGE DIS PLZ
+      }
+     
+      public ShooterStates getCurrentState() {
+        // CHANGE DIS PLZ
+        return null;
+      }
+
+        // ||||||||||||||||||||||||||||||||
+
+    }
+
+    
